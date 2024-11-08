e43ec5a1a68c2b58d216253f44f356d1186adf94
ROllerozxa <rollerozxa@voxelmanip.se>
2024-11-08 21:56:57 +0100

    `minetest` -> `core`

--- init.lua
+++ init.lua
@@ -1,15 +1,15 @@
 
-if minetest.get_mapgen_setting('mg_name') ~= "singlenode" then
+if core.get_mapgen_setting('mg_name') ~= "singlenode" then
 	error("Skygrid mod enabled, but not singlenode mapgen. Please disable this mod.")
 end
 
-local interval_x = minetest.settings:get("skygrid_interval_x") or 3
-local interval_y = minetest.settings:get("skygrid_interval_y") or 4
-local interval_z = minetest.settings:get("skygrid_interval_z") or 3
+local interval_x = core.settings:get("skygrid_interval_x") or 3
+local interval_y = core.settings:get("skygrid_interval_y") or 4
+local interval_z = core.settings:get("skygrid_interval_z") or 3
 
 local list_of_nodes = {}
-minetest.register_on_mods_loaded(function() -- Delay until all nodes are registered (mod loading complete)
-	for name, def in pairs(minetest.registered_nodes) do
+core.register_on_mods_loaded(function() -- Delay until all nodes are registered (mod loading complete)
+	for name, def in pairs(core.registered_nodes) do
 		if def
 		and def.groups									-- Exclude nodes without a group, which usually means indestructible ones
 		and def.groups.not_in_creative_inventory ~= 1	-- Exclude technical blocks.
@@ -41,15 +41,15 @@ function seeded_rng(seed)
 end
 
 local mapperlin
-minetest.after(0, function()
-	mapperlin = minetest.get_perlin(0, 1, 0, 1)
+core.after(0, function()
+	mapperlin = core.get_perlin(0, 1, 0, 1)
 end)
 
 local data = {}
 
-minetest.register_on_generated(function(minp, maxp, blockseed)
+core.register_on_generated(function(minp, maxp, blockseed)
 	local t0 = os.clock()
-	local vm, emin, emax = minetest.get_mapgen_object("voxelmanip")
+	local vm, emin, emax = core.get_mapgen_object("voxelmanip")
 	local area = VoxelArea:new{MinEdge = emin, MaxEdge = emax}
 	vm:get_data(data)
 
@@ -59,7 +59,7 @@ minetest.register_on_generated(function(minp, maxp, blockseed)
 		for y = minp.y, maxp.y do
 			for x = minp.x, maxp.x do
 				if (x % interval_x == 0) and (y % interval_y == 0) and (z % interval_z == 0) then
-					data[area:index(x, y, z)] = minetest.get_content_id(list_of_nodes[rng(1, #list_of_nodes)])
+					data[area:index(x, y, z)] = core.get_content_id(list_of_nodes[rng(1, #list_of_nodes)])
 				end
 			end
 		end
@@ -69,7 +69,7 @@ minetest.register_on_generated(function(minp, maxp, blockseed)
 	vm:write_to_map()
 end)
 
-minetest.register_on_newplayer(function(player)
+core.register_on_newplayer(function(player)
 	player:set_velocity({ x = 0, y = 0, z = 0 })
 	player:set_pos({ x = 0, y = 0.5, z = 0 })
 end)
